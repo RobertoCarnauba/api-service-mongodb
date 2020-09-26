3364aedd69c0773443aa5d396828c6be6b08a750
RobertoCarnauba <betocarnauba@hotmail.com>
2020-09-25 21:56:37 -0300

    Project create

---
+++ src/main/java/com/malutech/servicesales/ApiServiceSalesMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.malutech.servicesales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceSalesMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServiceSalesMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malutech/servicesales/ApiServiceSalesMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malutech.servicesales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceSalesMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
